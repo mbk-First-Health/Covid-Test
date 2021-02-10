b97aceeae5b68b828096776730a95096aef7663c
imam afgon madani <xsis.imamafgonm@xl.co.id>
2021-02-10 20:27:51 +0700

    complete crud

--- models/customer.js
+++ models/customer.js
@@ -1,17 +1,8 @@
 const conn = require('../connection/connect').connection
 
 class Customer {
-    constructor(nama, alamat, umur, jk, noHp, suhu) {
-        this.nama = nama
-        this.alamat = alamat
-        this.umur = umur
-        this.jeniKelamin = jk
-        this.noHp = noHp
-        this.suhu = suhu
-    }
-
     static findAll() {
-        const query = 'select * from customers'
+        const query = "select * from customers"
         return new Promise((resolve, reject) => {
            conn.query(query, function(err, res, field) {
                     if(err) {
@@ -21,6 +12,59 @@ class Customer {
            })
         })
     }
+
+    static findByName(name) {
+        const query = "select * from customer where nama like '%?%'"
+        return new Promise((resolve, reject) => {
+            conn.query(query, [name], function(err, res, field) {
+                if (err) {
+                    reject(err)
+                }
+                resolve({res, field})
+            })
+        })
+    }
+
+    static findbyId(id) {
+        const query = "select * from customers where id = ?"
+        return new Promise((resolve, reject) => {
+            conn.query(query, id, function(err, res, field) {
+                if (err) {
+                    reject(err)
+                }
+                resolve(res)
+            })
+        })
+    }
+
+    static create(data) {
+        let { jenisKelamin,noHp, ...cust} = data
+        cust.jenis_kelamin = jenisKelamin
+        cust.no_hp = noHp
+        const query = "insert into customers set ?"
+        return new Promise((resolve, reject) => {
+            conn.query(query, cust, function(err, res, field) {
+                if (err) {
+                    reject(err)
+                }
+                resolve({res, field})
+            })
+        })
+    }
+
+    static update(data) {
+        let {id, ...cust} = data
+        const query = "update customers set ? where id = ?"
+        return new Promise((resolve, reject) => {
+            conn.query(query, [cust, id], function(err, res, field) {
+                if (err) {
+                    reject(err)
+                }
+                resolve(res)
+            })
+        })
+    }
+
 }
 
 module.exports = Customer
