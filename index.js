ec80b41f103b93e6cea6aab520808ac1f45837e2
imam afgon madani <xsis.imamafgonm@xl.co.id>
2021-02-10 19:15:47 +0700

    try dummy controller

--- index.js
+++ index.js
@@ -2,11 +2,16 @@ const express = require('express')
 const app = express()
 const port = 3000
 const conn = require('./connection/connect')
+const CustomerController =  require('./controllers/customerController')
 
 conn.run()
 
 app.all('/*', (req, res) => {
-    res.send('SANGE OMMMM!!');
+    CustomerController.getCustomers()
+                    .then( result => {
+                        res.send(result);
+                    })
+                    .catch(console.log)
 });
 
 app.listen(port, () => {
