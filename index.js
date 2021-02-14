d6279101ee284be5fcb421512c691e51e117e32a
gon <iafgon@yahoo.com>
2021-02-14 16:32:33 +0700

    add cors

--- index.js
+++ index.js
@@ -2,12 +2,14 @@ const express = require('express')
 const app = express()
 const port = 3000
 const conn = require('./connection/connect')
+const cors = require('cors')
 const router =  require('./routes')
 const errHandler = require('./helper/errHandler')
 
 conn.run()
 app.use(express.json())
 app.use(express.urlencoded({extended: false}))
+app.use(cors())
 app.use(router)
 app.use(errHandler)
 
