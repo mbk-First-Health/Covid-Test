35ef341bc5fcb4bb76fbe96162f1f14a2e738e5e
gon <iafgon@yahoo.com>
2021-02-16 01:46:36 +0700

    clean up

--- helper/errHandler.js
+++ helper/errHandler.js
@@ -1,12 +1,15 @@
 module.exports = function name(err, req, res, next) {
-    switch (err.message) {
-        case 'not found':
+	console.log(err)
+
+	if (err.message == 'not found') {
             res.status(404).json({
                 msg: "pelanggan dengan id " + res.locals.id + " tidak ditemukan"
             })
-            break;
-    
-        default:
-            break;
-    }
+	} else if (err.code == 'ER_NO_DEFAULT_FOR_FIELD') {
+		const field = err.sqlMessage.split(" ")[1];
+		res.status(400).json({
+			msg: "field " + field + " tidak boleh kosong!"
+		})
+	}
+
 }
