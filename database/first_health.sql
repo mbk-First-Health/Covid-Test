e56f36bed1e8e10c66b00d5ca7c34ea35f9c902f
gon <iafgon@yahoo.com>
2021-02-16 01:46:14 +0700

    add field keterangan, jenis_sample, dll and set to mysql to strict_mode

--- database/first_health.sql
+++ database/first_health.sql
@@ -1,24 +1,27 @@
+SET GLOBAL sql_mode = 'NO_AUTO_CREATE_USER,NO_ENGINE_SUBSTITUTION,STRICT_ALL_TABLES';
 create database first_health;
 
 use first_health;
 create table customers (
 id int NOT NULL AUTO_INCREMENT,
-nama varchar(100),
-umur int,
-jenis_kelamin varchar(20),
-alamat varchar(100),
-no_hp varchar(15),
-suhu decimal,
+nama varchar(100) NOT NULL,
+umur int NOT NULL,
+jenis_kelamin varchar(20) NOT NULL,
+alamat varchar(100) NOT NULL,
+no_hp varchar(15) NOT NULL,
+suhu decimal NOT NULL,
+jenis_sample varchar(100) NOT NULL,
 primary key(id)
 );
 create table test_result (
 id int NOT NULL AUTO_INCREMENT,
 hasil enum('Positif', 'Negatif'),
 tanggal date,
+nilai_rujukan varchar(100) NOT NULL,
+jenis_pemeriksaan varchar(100) NOT NULL,
+keterangan varchar(255) NOT NULL,
 id_customer int,
 PRIMARY KEY (id),
 FOREIGN KEY (id_customer) REFERENCES customers(id)
 );
 
-select * from customers;
-select * from test_result;
